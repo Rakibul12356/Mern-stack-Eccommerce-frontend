61b4bac9913b2eb5c1865a9afbf202ac95bf1fc1
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-21 14:50:45 +0600

    nvabar ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
  
   return (
     <>
+    <Navbar></Navbar>
       <h2 className='text-3xl text-red-700'>Hello World</h2>
       <Outlet></Outlet>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {RouterProvider,} from "react-router-dom";
 import './index.css'
+import 'remixicon/fonts/remixicon.css'
 
 import router from "./routers/Router.jsx";
 
