85948e662f32909153e1c2d9eaf48e5705ed13bb
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-21 15:33:14 +0600

    Category section create

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Banner from './Banner'
+import Category from './Category'
 
 const Home = () => {
   return (
    <>
    <Banner></Banner>
+   <Category></Category>
    </>
   )
 }
