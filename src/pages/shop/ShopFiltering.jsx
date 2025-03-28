1f45fc101d6b6c3d0c790df53d88351f381355ef
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-03-29 03:34:04 +0600

    filtered by colors funcutionality add

--- src/pages/shop/ShopFiltering.jsx
+++ src/pages/shop/ShopFiltering.jsx
@@ -4,6 +4,7 @@ const ShopFiltering = ({ filters, filtersState, setFiltersState, clearFilters })
     return (
         <div className='space-y-5 flex-shrink-0'>
             <h3>Filters</h3>
+            {/**category */}
             <div className='flex flex-col space-y-2'>
                 <h4 className='font-medium text-lg'>Category</h4>
                 <hr />
@@ -20,6 +21,29 @@ const ShopFiltering = ({ filters, filtersState, setFiltersState, clearFilters })
                                 }
                             />
                             <span className='ml-1'>{category}</span>
+                        </label>
+                    )
+                    )
+                }
+
+            </div>
+             {/**color*/}
+             <div className='flex flex-col space-y-2'>
+                <h4 className='font-medium text-lg'>Colors</h4>
+                <hr />
+                {
+                    filters.colors.map((color) => (
+                        <label key={color} className='capitalize cursor-pointer '>
+                            <input type="radio" name="category" id="category"
+                                value={color}
+                                checked={filtersState.color === color}
+                                onChange={(e) => setFiltersState({
+                                    ...filtersState, color: e.target.value
+                                }
+                                )
+                                }
+                            />
+                            <span className='ml-1'>{color}</span>
                         </label>
                     )
                     )
