9a99dddaf4affc5a30f011e945969b128fbd3100
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-04-20 18:41:39 +0600

    Modal delet product and payment deisgine

--- src/pages/shop/CartModal.jsx
+++ src/pages/shop/CartModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import OrderSummary from './OrderSummary';
 
 const CartModal = ({ products, isOpen, onClose }) => {
     console.log(products.length)
@@ -8,12 +9,13 @@ const CartModal = ({ products, isOpen, onClose }) => {
             <div className={`fixed right-0 top-0 md:w-1/3 w-full bg-white h-full overflow-y-auto transition-transform ${isOpen ? "translate-x-0" : "translate-x-full"}`}
                 style={{ transition: 'transform 300ms cubic-bezier(0.25,0.46,0.45,0.94' }}>
                 <div className='p-4 mt-4'>
-                    <div className='flex justify-between items-center'>
+                    <div className='flex justify-between items-center mb-4'>
                         <h4 className='text-xl font-semibold'>Your Cart</h4>
                         <button
                             onClick={() => onClose()} className='text-gray-600 hover:text-gray-900'><i className="ri-xrp-line bg-black p-1 text-white"></i></button>
                     </div>
-                    <div>
+                    {/**cart details */}
+                    <div className='cart-items'>
                         {
                             products.length === 0 ? (<div>Your cart is empty</div>) : (
                                 products.map((item, index) => (
@@ -29,12 +31,29 @@ const CartModal = ({ products, isOpen, onClose }) => {
                                                     ${Number(item.price).toFixed(2)}
                                                 </p>
                                             </div>
+                                            <div className='flex flex-row md:justify-start justify-end items-center mt-2'>
+                                                <button className='size-6 flex items-center justify-center px-1.5 rounded-full
+                                                bg-gray-200 text-gray-700 hover:bg-primary     hover:text-white ml-8'>-</button>
+                                                <span className='px-2 text-center mx-1'>{item.quantity}</span>
+                                                <button className='size-6 flex items-center justify-center px-1.5 rounded-full
+                                                bg-gray-200 text-gray-700 hover:bg-primary     hover:text-white'>+</button>
+                                                <div className='ml-5 '>
+                                                    <button className='text-red-500 hover:text-red-8000'>Remove</button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 ))
                             )
                         }
                     </div>
+                    {/**calculation */}
+                        {
+                            products.length > 0 && (
+                                <OrderSummary></OrderSummary>
+                            )
+                        }
+
                 </div>
             </div>
         </div>
